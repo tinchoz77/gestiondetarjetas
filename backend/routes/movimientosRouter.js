64a126e44a352dc15dbfb0c67888b70595397150
tinchoz77 <tinchoz@gmail.com>
2020-09-05 11:32:57 -0300

    Movimientos: no permitir modificarlos ni eliminarlos una vez dados de alta

--- backend/routes/movimientosRouter.js
+++ backend/routes/movimientosRouter.js
@@ -2,13 +2,13 @@ const express = require("express")
 const Movimiento = require("../models/Movimiento")
 const movimientosRouter = express.Router()
 
-// Obtener todos los clientes
+// Obtener todos los movimientos
 movimientosRouter.get("/movimientos", async (req, res) => {
     const movimientos = await Movimiento.find()
     res.status(200).send(movimientos);
 })
 
-// Obtener un cliente particular
+// Obtener un movimiento particular
 movimientosRouter.get("/movimientos/:id", async (req, res) => {
     try {
         const movimiento = await Movimiento.findOne({ _id: req.params.id });
@@ -22,7 +22,7 @@ movimientosRouter.get("/movimientos/:id", async (req, res) => {
     }
   })
 
-// Crear un nuevo cliente
+// Crear un nuevo movimiento
 movimientosRouter.post("/tarjetas/:id/movimientos", async (req, res) => {
     const movimiento = new Movimiento({
         cliente: req.body.cliente,
@@ -38,45 +38,18 @@ movimientosRouter.post("/tarjetas/:id/movimientos", async (req, res) => {
     res.send(movimiento);
 })
 
-// Actualizar todos los datos del cliente
+// No se permite actualizar todos los datos del movimiento
 movimientosRouter.put("/movimientos/:id", async (req, res) => {
-    try {
-        const movimiento = await Movimiento.findOne({ _id: req.params.id });
-        if (movimiento == null) {
-            res.sendStatus(404);
-        } else {
-            movimiento.cliente = req.body.cliente;
-            movimiento.tarjeta = req.body.tarjeta;
-            movimiento.fecha = req.body.fecha;
-            movimiento.descripcion = req.body.descripcion;
-            movimiento.importe = req.body.importe;
-            movimiento.activo = req.body.activo;
-                
-            await movimiento.save();
-            res.status(200).send(movimiento);
-        }
-    } catch(CastError) {
-        res.sendStatus(404);
-    }
+    res.sendStatus(403);
 })
 
-// Actualizar algunos de los datos del cliente
+// Actualizar el estado del movimiento
 movimientosRouter.patch("/movimientos/:id", async (req, res) => {
     try {
         const movimiento = await Movimiento.findOne({ _id: req.params.id });
         if (movimiento == null) {
             res.sendStatus(404);
         } else {
-            if (req.body.cliente)
-                movimiento.cliente = req.body.cliente;
-            if (req.body.tarjeta)
-                movimiento.tarjeta = req.body.tarjeta;
-            if (req.body.fecha)
-                movimiento.fecha = req.body.fecha;
-            if (req.body.descripcion)
-                movimiento.descripcion = req.body.descripcion;
-            if (req.body.importe)
-                movimiento.importe = req.body.importe;
             if (req.body.activo)
                 movimiento.activo = req.body.activo;
             
@@ -88,19 +61,9 @@ movimientosRouter.patch("/movimientos/:id", async (req, res) => {
     }
 })
 
-// Eliminar un cliente devolviendo el documento eliminado
+// No se permite eliminar un movimiento
 movimientosRouter.delete("/movimientos/:id", async (req, res) => {
-    try {
-        const movimiento = await Movimiento.findOne({ _id: req.params.id });
-        if (movimiento == null) {
-            res.sendStatus(404);
-        } else {
-            await Movimiento.deleteOne({ _id: req.params.id });
-            res.status(200).send(movimiento);
-        }
-    } catch(CastError) {
-        res.sendStatus(404);
-    }
+    res.sendStatus(403);
 })
 
 
